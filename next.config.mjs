2d225bbda9a3c5aa849744f68325b19c02a049fc
amercosic21 <amercoski@gmail.com>
2026-06-29 04:38:04 +0200

    updating supabase keys

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "wovhxzpcjuroxlheabgv.supabase.co",
+        hostname: "wmmnuqmroxwwevktioio.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
